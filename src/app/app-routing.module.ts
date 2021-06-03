d68771d98cbbbba74e54e58d9ca2c8343ae21057
garima sharma <garimaa.sharma@tcs.com>
2021-06-03 17:14:58 +0530

    initial routing set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './Main/homepage/homepage.component';
+import { LoginComponent } from './Main/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component : LoginComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: '',redirectTo:'/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
